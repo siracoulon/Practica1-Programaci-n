cd063b0cdd7e82683dd9154ffd5a0876c1540787
siracoulon <sira.coulon@edu.uah.es>
2025-03-12 17:24:22 +0100

    commit merge mio y de Sara

---
+++ Tests/es/uah/matcomp/mp/e1/ejerciciosclases/ej6_5/ResizableTest.java
@@ -0,0 +1,12 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases.ej6_5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResizableTest {
+
+    @Test
+    void resize() {
+    }
+}
--- src/es/uah/matcomp/mp/e1/ejerciciosclases/ej2_9/MyTriangle.java
+++
@@ -1,4 +0,0 @@
-package es.uah.matcomp.mp.e1.ejerciciosclases.ej2_9;
-
-public class MyTriangle {
-}
